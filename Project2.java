a9e38231a4dcc7c208c9703b1e0a28e2c4a6dbed
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-05-22 22:41:38 +0300

    Create Project2.java

---
+++ Project2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double urunFiyati, kdvOrani, kdvTutari;
+        System.out.println("--- KDV Hesaplama Programı ---");
+        System.out.print("Ürünün Fiyatını Giriniz : ");
+        urunFiyati = input.nextDouble();
+        System.out.println("");
+        kdvOrani = (urunFiyati >= 1000 ? 0.08 : 0.18);
+        kdvTutari = urunFiyati * kdvOrani;
+        System.out.println("KDV'siz Fiyat : " + urunFiyati + " TL");
+        System.out.println("KDV Oranı : " + (kdvOrani == 0.18 ? "18%" : "8%"));
+        System.out.println("KDV Tutarı : " + kdvTutari + " TL");
+        System.out.println("KDV'li Fiyat : " + (urunFiyati + kdvTutari) + " TL");
+        System.out.println("-------------------------------");
+    }
+}
