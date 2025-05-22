ab0f54dbc328e660005bc95303ad64e440678f85
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-05-22 22:20:44 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double mat, fizik, kimya, turkce, tarih, muzik, ortalama;
+        System.out.println("--- Not Ortalaması Hesaplayan Program ---");
+        System.out.print("Matematik Notunuzu Giriniz : ");
+        mat = input.nextDouble();
+        System.out.print("Fizik Notunuzu Giriniz : ");
+        fizik = input.nextDouble();
+        System.out.print("Kimya Notunuzu Giriniz : ");
+        kimya = input.nextDouble();
+        System.out.print("Türkçe Notunuzu Giriniz : ");
+        turkce = input.nextDouble();
+        System.out.print("Tarih Notunuzu Giriniz : ");
+        tarih = input.nextDouble();
+        System.out.print("Müzik Notunuzu Giriniz : ");
+        muzik = input.nextDouble();
+        System.out.println("--------------------------------------");
+        ortalama = (mat + fizik + kimya + turkce + tarih + muzik) / 6;
+        System.out.println("Ortalamanız : " + ortalama + "\n");
+        System.out.println("Durum: " + (ortalama >= 60 ? "Sınıfı Geçti" : "Sınıfta Kaldı"));
+        System.out.println("--------------------------------------");
+
+    }
+}
