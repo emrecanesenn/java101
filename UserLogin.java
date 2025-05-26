72c0dac260f1d7d3ac6832f810179978700d4ba8
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-05-26 18:30:17 +0300

    Create UserLogin.java

---
+++ UserLogin.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String kullaniciAdi, sifre;
+        System.out.println("----- Kullanıcı Paneli -----");
+        System.out.print("Kullanıcı Adını Giriniz: ");
+        kullaniciAdi = input.nextLine();
+        System.out.print("Şifrenizi Giriniz: ");
+        sifre = input.nextLine();
+        if(kullaniciAdi.equals("Schedarp") && sifre.equals("birikiuc")) {
+            System.out.println("Giriş Başarılı!");
+        } else if (!sifre.equals("birikiuc") && kullaniciAdi.equals("Schedarp")) {
+            System.out.println("Şifreniz Hatalı!");
+            System.out.print("Şifrenizi Sıfırlamak İster misiniz ? (E/H) : ");
+            String cevap = input.nextLine();
+            if(cevap.equals("E") || cevap.equals("e")) {
+                System.out.print("Yeni Şifrenizi Giriniz: ");
+                sifre = input.nextLine();
+                if(sifre.equals("birikiuc")) {
+                    System.out.println("Şifre Sıfırlama Başarısız!");
+                }
+                else {
+                    System.out.println("Şifre Sıfırlama Başarılı!");
+                }
+            }
+        }
+        else {
+            System.out.println("Giriş Başarısız!");
+        }
+        System.out.println("----- Kullanıcı Paneli -----");
+    }
+}
