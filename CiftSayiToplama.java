cfc835e04ff303b4f2fe4c3ba35bef035910d9ca
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-05-27 03:43:11 +0300

    Create CiftSayiToplama.java

---
+++ CiftSayiToplama.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int toplam = 0;
+        int sayi;
+        
+        do {
+            System.out.print("Bir sayı giriniz: ");
+            sayi = scanner.nextInt();
+
+            if (sayi % 4 == 0) {
+                toplam += sayi;
+            }
+            
+        } while (sayi % 2 == 0); // Tek sayı girilene kadar devam et
+        
+        System.out.println("Girilen 4'ün katı olan sayıların toplamı: " + toplam);
+        
+        scanner.close();
+    }
+}
