1800d56d5e3c763e1a903067556e48178fc5657e
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-05-27 03:36:28 +0300

    Create DonguOrtalama.java

---
+++ DonguOrtalama.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("----- Döngüler -----");
+        int sayi, ortalama, toplam = 0, sayac = 0;
+        System.out.print("Sayı Giriniz : ");
+        sayi = scanner.nextInt();
+        for(int i = 0; i <= sayi; i++) {
+            if(i % 3 == 0 && i % 4 == 0) {
+                System.out.println(i + ", ");
+                toplam += i;
+                sayac++;
+            };
+        }
+        ortalama = toplam / sayac;
+        System.out.println("3'e ve 4'e bölünen sayıların ortalaması : " + ortalama);
+        System.out.println("----- Döngüler -----");
+
+        scanner.close();
+    }
+}
