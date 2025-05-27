4d4912573602949f573f88fe2117f6105f1bb322
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-05-27 03:04:24 +0300

    Create ArtikYil.java

---
+++ ArtikYil.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int yil;
+        System.out.println("----- Artık Yıl Hesaplama -----");
+        
+        System.out.print("Yıl Giriniz : ");
+        yil = scanner.nextInt();
+        
+        System.out.println("\nGirdiğiniz yıl: " + (yil % 400 == 0 ? "Artık yıldır." : "Artık yıl değildir."));
+        System.out.println("----- Artık Yıl Hesaplama -----");
+        
+        scanner.close();
+    }
+}
