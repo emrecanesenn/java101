04e1ace4023c34dbd620b2c897282ea9efe81dca
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-05-28 17:24:41 +0300

    Create ElmasOlustur.java

---
+++ ElmasOlustur.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Elmasın boyutunu giriniz (tek sayı): ");
+        int n = scanner.nextInt();
+        
+        // Üst yarım
+        for (int i = 1; i <= n; i += 2) {
+            // Boşlukları yazdır
+            for (int j = 0; j < (n - i) / 2; j++) {
+                System.out.print(" ");
+            }
+            // Yıldızları yazdır
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        // Alt yarım
+        for (int i = n - 2; i >= 1; i -= 2) {
+            // Boşlukları yazdır
+            for (int j = 0; j < (n - i) / 2; j++) {
+                System.out.print(" ");
+            }
+            // Yıldızları yazdır
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        scanner.close();
+    }
+}
