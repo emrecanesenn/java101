8a9b46ab7e20e3f1a7086b617d84fe4a98a45a5c
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-05-31 01:19:35 +0300

    Create RecursivePower.java

---
+++ RecursivePower.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int hesapla(int base, int power) {
+        if(power == 0) return 1;
+        else return base * hesapla(base, power - 1);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---- Üslü Sayı Hesaplayan Program ----\n");
+
+        System.out.print("Taban Değerini Giriniz : ");
+        int base = scanner.nextInt();
+        System.out.print("Üs Değerini Giriniz : ");
+        int power = scanner.nextInt();
+
+        System.out.println("\nSonuç: " + hesapla(base, power));
+
+        scanner.close();
+    }
+}
