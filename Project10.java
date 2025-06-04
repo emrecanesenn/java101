e9e647e729866cef81bc02c31f41bb13e6b994b0
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-06-04 03:14:12 +0300

    Create Project10.java

---
+++ Project10.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Dizinin boyutunu giriniz: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print((i + 1) + ". Elemanı Giriniz: ");
+            arr[i] = input.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println("---- Sıralanmış Liste ----");
+        for(int siralama: arr) {
+            System.out.println("-> " + siralama);
+        }
+
+    }
+}
