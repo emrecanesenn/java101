4ce350e6003e84e07ffa5bec3206dc107c82fab9
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-06-05 19:48:18 +0300

    Update UsluHesaplama.java

--- UsluHesaplama.java
+++ UsluHesaplama.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class UsluHesaplama {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int sayi, toplam = 1, us;
