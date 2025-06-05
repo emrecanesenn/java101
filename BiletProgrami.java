6a8e8d1a8afa3583dd0b2a175901f7794ee50093
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-06-05 19:41:38 +0300

    Update BiletProgrami.java

--- BiletProgrami.java
+++ BiletProgrami.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class BiletProgrami {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int km, yas, yolculukTuru; double biletFiyati, indirimsizBiletFiyati;
