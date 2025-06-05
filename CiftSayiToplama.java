5f9f606a37a2cf9926eab074182626a902acd4d8
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-06-05 19:42:10 +0300

    Update CiftSayiToplama.java

--- CiftSayiToplama.java
+++ CiftSayiToplama.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class CiftSayiToplama {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int toplam = 0;
