805071d9872ba857d787e022d695552d172e9c18
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-06-05 19:46:58 +0300

    Update Project8.java

--- Project8.java
+++ Project8.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Project8 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int sayi1, sayi2, islem;
