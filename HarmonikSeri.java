2673f085d19c73674e64ff303e47d4feb954282b
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-06-05 19:43:44 +0300

    Update HarmonikSeri.java

--- HarmonikSeri.java
+++ HarmonikSeri.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class HarmonikSeri {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("---- Harmonik Seri Hesaplama ----");
