3a0e99366a2a274eda83840866afdf1ce1914f0e
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-06-05 19:45:16 +0300

    Update OrtalamaHesaplama.java

--- OrtalamaHesaplama.java
+++ OrtalamaHesaplama.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class OrtalamaHesaplama {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double matematik, fizik, turkce, kimya, muzik, toplam = 0;
