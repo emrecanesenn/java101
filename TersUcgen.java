d72283177668a9bbda7422fae09732ae596583cf
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-06-05 19:47:57 +0300

    Update TersUcgen.java

--- TersUcgen.java
+++ TersUcgen.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class TersUcgen {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("---- Ters Üçgen ----");
