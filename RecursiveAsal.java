63450c3311a4323fce1695750c7ab848c7a53251
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-06-05 19:47:17 +0300

    Update RecursiveAsal.java

--- RecursiveAsal.java
+++ RecursiveAsal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class RecursiveAsal {
     public static String hesapla(int sayi) {
         return asalKontrol(sayi, sayi - 1) ? "Asal sayıdır." : "Asal değildir.";
     }
