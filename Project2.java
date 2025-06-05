40cf101c7009ee33ee41bb0c9c92e70f4c53bcca
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-06-05 19:46:01 +0300

    Update Project2.java

--- Project2.java
+++ Project2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Project2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double urunFiyati, kdvOrani, kdvTutari;
