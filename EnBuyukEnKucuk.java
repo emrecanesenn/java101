4dc326f0717dfbd2019e8d2c32796ad1f0ef2367
Emrecan Esen <111984793+emrecanesenn@users.noreply.github.com>
2025-06-05 19:43:06 +0300

    Update EnBuyukEnKucuk.java

--- EnBuyukEnKucuk.java
+++ EnBuyukEnKucuk.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class EnBuyukEnKucuk {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n, enBuyuk = 0, enKucuk = 0;
